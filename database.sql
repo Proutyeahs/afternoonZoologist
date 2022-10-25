235eaf4b64ba7e8a83e8e7495b5774d38dfaf13e
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-10-25 14:28:31 -0500

    added db sql

--- database.sql
+++ database.sql
@@ -1,10 +1,66 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "username" VARCHAR (99) UNIQUE NOT NULL,
+    "password" VARCHAR (999) NOT NULL,
+    "mod" BOOLEAN DEFAULT false,
+    "admin" BOOLEAN DEFAULT false
+);
+
+CREATE TABLE "type" (
+	"id" SERIAL PRIMARY KEY,
+	"type" VARCHAR (999) NOT NULL,
+	"description" VARCHAR (9999) NOT NULL
+);
+
+CREATE TABLE "monster" (
+	"id" SERIAL PRIMARY KEY,
+	"monster" VARCHAR (999) NOT NULL,
+	"description" VARCHAR (9999) NOT NULL,
+	"type_id" INT REFERENCES "type" NOT NULL
+);
+
+CREATE TABLE "weapon" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (999) NOT NULL,
+	"description" VARCHAR (9999) NOT NULL
+);
+
+CREATE TABLE "weapon_inventory" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user" NOT NULL,
+	"weapon_id" INT REFERENCES "weapon" NOT NULL,
+	"type_id" INT REFERENCES "type" NOT NULL,
+	"att" INT NOT NULL
+);
+
+CREATE TABLE "monster_collection" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INT REFERENCES "user" NOT NULL,
+    "monster_id" INT REFERENCES "monster" NOT NULL,
+    "hp" INT NOT NULL,
+    "att" INT NOT NULL,
+    "def" INT NOT NULL,
+    "lvl" INT NOT NULL,
+    "exp" INT NOT NULL,
+    "weapon_inventory_id" INT REFERENCES "weapon_inventory" NOT NULL,
+    "effect" BOOLEAN DEFAULT false
+);
+
+CREATE TABLE "effect" (
+	"id" SERIAL PRIMARY KEY,
+	"effect" VARCHAR (999) NOT NULL,
+	"description" VARCHAR (9999) NOT NULL
+);
+
+CREATE TABLE "item" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (999) NOT NULL,
+	"description" VARCHAR (9999) NOT NULL,
+	"effect_id" INT REFERENCES "effect" NOT NULL
+);
+
+CREATE TABLE "item_inventory" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user" NOT NULL, 
+	"item_id" INT REFERENCES "item" NOT NULL
 );
