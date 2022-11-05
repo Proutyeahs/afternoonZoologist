c58450ce725985ca68fdf6b9e4ceb2e8df8de3d3
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-11-05 12:43:58 -0500

    map saga

--- src/components/Map/Grid.jsx
+++ src/components/Map/Grid.jsx
@@ -1,12 +1,29 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import userReducer from "../../redux/reducers/user.reducer";
+
 function Grid() {
 
-    let number = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
+    const dispatch = useDispatch()
 
+    const user = useSelector((store) => store.user)
+
+    useEffect(() => {
+        dispatch({
+            type: "GET_MAP",
+            payload: user.id
+        })
+    }, []);
+
+    let number = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 4 }];
     return (
         <>
-            {number.map(num => (
-                <div className="imageGrid" key={num.id}>{num.id}</div>
-            ))}
+            <div className="grid">
+                {number.map(num => (
+                    <div className="imageGrid"></div>
+                ))}
+            </div>
         </>
     )
 }
