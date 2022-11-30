d652408bcccd1e7c191353cbee73582856fcf4cb
Eric Prouty <Eric.W.Prouty@gmail.com>
2022-11-30 12:57:16 -0600

    user location marked

--- src/components/Map/Grid.jsx
+++ src/components/Map/Grid.jsx
@@ -6,10 +6,9 @@ function Grid() {
 
     const dispatch = useDispatch()
 
-    const user = useSelector((store) => store.user)
-
     // userLocation and mapInfo from the database 
     const map = useSelector((store) => store.map)
+    const user = useSelector((store) => store.user)
 
     useEffect(() => {
         dispatch({
@@ -25,8 +24,8 @@ function Grid() {
                 {/* world map 1st draft */}
                 <img className="map" src="https://res.cloudinary.com/dzyea2237/image/upload/v1669783170/PXL_20221130_043409125_xk3sky.jpg" />
 
-                {map.map(num => (
-                    <div key={num.id} className="imageGrid"></div>
+                {map.map(grid => (
+                    <div key={grid.id} className={`imageGrid ${user.location === grid.id ? "location" : ""}`}></div>
                 ))}
             </div>
         </>
